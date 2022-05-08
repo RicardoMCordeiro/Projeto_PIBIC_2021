9c09f82713c5ad788b8c2fe725b80a8613def8d0
RicardoMCordeiro <104388910+RicardoMCordeiro@users.noreply.github.com>
2022-05-07 21:13:30 -0300

    update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,33 +1,38 @@
 export const years = () => {
-    const list = [];
+  const list = [];
   
-    for (let i = 2016; i <= 2020; ++i) {
-      list.push(i.toString());
-    }
-    return list;
-  };
-  
-  export const optionsMap = (op) => {
-    if (op === "31/03") return "1";
-    else if (op === "30/06") return "2";
-    else if (op === "30/09") return "3";
-    else return "4";
-  };
-  
-  export const formatMoney = (val, hasSymbol, scale = 1000) => {
-    const num = parseFloat(val) * scale;
-  
-    if (hasSymbol) {
-      return num.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
-    } else {
-      return num.toLocaleString("pt-BR", { minimumFractionDigits: 2 });
-    }
-  };
-  
-  export const hasArgs = (query) => Object.keys(query).length > 0;
-  
-  export const formatPhone = (ddd, num) => {
-    if(!ddd || !num) return null;
-  
-    return `(${ddd}) ${num}`
+  for (let i = 2016; i <= 2020; ++i) {
+    list.push(i.toString());
   }
+  return list;
+};
+  
+export const optionsMap = (op) => {
+  if (op === "31/03") return "0";
+  else if (op === "30/06") return "1";
+  else if (op === "30/09") return "2";
+  else if (op === "31/12") return "3";
+};
+
+export const adjustmentStatement = (demonstrative) => {
+  if (demonstrative === "Consolidada") return "con";
+  else if (demonstrative === "Individual") return "ind";
+};
+  
+export const formatMoney = (val, hasSymbol, scale = 1000) => {
+  const num = parseFloat(val) * scale;
+  
+  if (hasSymbol) {
+    return num.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+  } else {
+    return num.toLocaleString("pt-BR", { minimumFractionDigits: 2 });
+  }
+};
+  
+export const hasArgs = (query) => Object.keys(query).length > 0;
+  
+export const formatPhone = (ddd, num) => {
+  if(!ddd || !num) return null;
+
+  return `(${ddd}) ${num}`
+}
